1ddc916481b026cd87173ef12f96ab9c8b685dfe
Waptech <abdulfataiyakub07@gmail.com>
2024-01-01 19:27:27 +0100

    updated the path for the weather display component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import WeatherDisplay from './WeatherDisplay';
+import WeatherDisplay from './components/WeatherDisplay';
 
 const WeatherApp = () => {
   const [location, setLocation] = useState('');
